26c78de951e12dfc33f44cd5bd56172c2af4df29
Mofazzal-145 <md.mofazzalhossain145@gmail.com>
2022-03-17 14:43:28 +0600

    import folder

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
 import { useEffect, useState } from 'react';
+import Countries from './components/Countries/Countries';
+import Person from './components/Person/Person';
+import Header from './components/Header/Header';
 
+// ex: 02
 function App() {
+  return(
+    <div className='App'>
+      <Header></Header>
+      <Countries></Countries>
+      <Person></Person>
+    </div>
+  )
+}
+
+// ex : 01
+/* function App() {
   return (
     <div className="App">
      <LoadCountries></LoadCountries>
@@ -24,9 +39,23 @@ function LoadCountries () {
     <div>
       <h1>Let's have tour all over the world</h1>
       <h3>All countries: {countries.length}</h3>
+      {
+        // countries.map(country => <p>{country.name.common}</p>)
+        countries.map(country => <Country name= {country.name.common} official ={country.name.official} population={country.population}></Country>)
+      }
+
     </div>
   )
 }
 
+function Country (props) {
+  return (
+    <div>
+      <h2>Name:{props.name}</h2>
+      <h3>Official: {props.official}</h3>
+      <h3>Population:{props.population}</h3>
+    </div>
+  )
+} */
 
 export default App;
---
+++ src/components/Countries/Countries.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Countries = () => {
+    return (
+        <div>
+            <h2>Countries in another file</h2>
+        </div>
+    );
+};
+
+export default Countries;
